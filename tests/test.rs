9801d8a74c8a1e86b45e8cbd5f62ef557168a7c4
Tyrone Tudehope <tyronetudehope@gmail.com>
2022-05-11 22:34:51 +0200

    Reset edition to 2018

--- tests/test.rs
+++ tests/test.rs
@@ -4,7 +4,7 @@ use hashfn::hashfn;
 ///
 /// ```
 /// pub(crate) const DO_A_THING_HASH: &str =
-///     "a429692d48481f8baed209e387949132f77fe7c3465e121ae31d7518db2f";
+///     "927d548953088d9e1e2e69ca1b5c10ada7a1a08aebade958b7fbf16ddc284a";
 /// pub(crate) fn do_a_thing() {}
 /// ```
 #[hashfn]
@@ -16,7 +16,7 @@ pub(crate) fn do_a_thing() {
 #[test]
 fn test_hashfn() {
     assert_eq!(
-        "5f7919f82b459da2a812694d479eae667797b2f36b4c8fe5e51d1aeb1fad78f",
+        "927d548953088d9e1e2e69ca1b5c10ada7a1a08aebade958b7fbf16ddc284a",
         DO_A_THING_HASH
     );
     do_a_thing();
